8482ad41706326920fad80de01106669de1a93ed
Lubwama-Emmanuel <92721634+Lubwama-Emmanuel@users.noreply.github.com>
2022-02-01 11:51:00 +0300

    intial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const nav = document.querySelector('.nav');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
